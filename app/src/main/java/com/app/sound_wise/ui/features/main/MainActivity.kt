d1aed9a4201308f9472a64c805437941d279e688
Rizki Sepriadi <muhammadrizkisepriadi@gmail.com>
2025-05-08 09:55:03 +0700

    feat: Add Apptheme and Navigation

--- app/src/main/java/com/app/sound_wise/ui/features/main/MainActivity.kt
+++ app/src/main/java/com/app/sound_wise/ui/features/main/MainActivity.kt
@@ -3,13 +3,27 @@ package com.app.sound_wise.ui.features.main
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.app.sound_wise.ui.AppContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.app.sound_wise.ui.SoundWiseApp
+import com.app.sound_wise.ui.theme.AppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
-            AppContent()
+            AppTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    SoundWiseApp()
+                }
+            }
         }
     }
 }
