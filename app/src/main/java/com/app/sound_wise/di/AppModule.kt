2b4764936af8391b45506ff3c905ca971d15a17e
Rizki Sepriadi <muhammadrizkisepriadi@gmail.com>
2025-05-08 09:57:42 +0700

    feat: add SplashViewModel and RuleRepository

--- app/src/main/java/com/app/sound_wise/di/AppModule.kt
+++ app/src/main/java/com/app/sound_wise/di/AppModule.kt
@@ -1,9 +1,14 @@
 package com.app.sound_wise.di
 
+import com.app.sound_wise.data.repositories.RuleRepository
 import com.app.sound_wise.ui.features.home.HomeViewModel
+import com.app.sound_wise.ui.features.splash.SplashViewModel
+import org.koin.android.ext.koin.androidContext
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val appModule = module {
-    viewModel { HomeViewModel() }
+    single { RuleRepository(androidContext()) }
+    viewModel { HomeViewModel(get()) }
+    viewModel { SplashViewModel() }
 }
