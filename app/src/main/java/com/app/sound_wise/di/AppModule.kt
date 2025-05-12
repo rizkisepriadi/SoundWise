398aea899600042ea5e72463927829597cee5cb1
Rizki Sepriadi <muhammadrizkisepriadi@gmail.com>
2025-05-12 13:30:43 +0700

    feat: add QuestionViewModel and LoadingViewModel

--- app/src/main/java/com/app/sound_wise/di/AppModule.kt
+++ app/src/main/java/com/app/sound_wise/di/AppModule.kt
@@ -2,13 +2,17 @@ package com.app.sound_wise.di
 
 import com.app.sound_wise.data.repositories.RuleRepository
 import com.app.sound_wise.ui.features.home.HomeViewModel
+import com.app.sound_wise.ui.features.loading.LoadingViewModel
 import com.app.sound_wise.ui.features.splash.SplashViewModel
+import com.app.sound_wise.ui.question.QuestionsViewModel
 import org.koin.android.ext.koin.androidContext
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val appModule = module {
     single { RuleRepository(androidContext()) }
+    viewModel{ QuestionsViewModel(get()) }
     viewModel { HomeViewModel(get()) }
     viewModel { SplashViewModel() }
+    viewModel { LoadingViewModel() }
 }
