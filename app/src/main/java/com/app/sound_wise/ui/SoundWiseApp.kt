d162978ab4984bded3f8819120ed661e072d1ea0
Rizki Sepriadi <muhammadrizkisepriadi@gmail.com>
2025-05-12 13:31:55 +0700

    feat: add Loading, Question Navigation and Result

--- app/src/main/java/com/app/sound_wise/ui/SoundWiseApp.kt
+++ app/src/main/java/com/app/sound_wise/ui/SoundWiseApp.kt
@@ -7,12 +7,15 @@ import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.app.sound_wise.ui.features.home.HomeScreen
+import com.app.sound_wise.ui.features.loading.LoadingScreen
+import com.app.sound_wise.ui.features.result.ResultScreen
 import com.app.sound_wise.ui.features.splash.SplashScreen
 import com.app.sound_wise.ui.navigation.Screen
-import androidx.compose.runtime.getValue
+import com.app.sound_wise.ui.question.QuestionsScreen
+import com.app.sound_wise.ui.question.QuestionsViewModel
+import org.koin.androidx.compose.koinViewModel
 
 
 @Composable
@@ -20,8 +23,7 @@ fun SoundWiseApp(
     modifier: Modifier = Modifier,
     navController: NavHostController = rememberNavController()
 ) {
-    val currentBackStackEntry by navController.currentBackStackEntryAsState()
-    val currentRoute = currentBackStackEntry?.destination?.route
+    val viewModel = koinViewModel<QuestionsViewModel>()
 
     Scaffold(
         topBar = {},
@@ -34,7 +36,37 @@ fun SoundWiseApp(
             modifier = Modifier.padding(innerPadding)
         ) {
             composable(Screen.Splash.route) { SplashScreen(navController) }
+            composable(Screen.Loading.route) { LoadingScreen(navController) }
             composable(Screen.Home.route) { HomeScreen() }
+            composable("${Screen.Question.route}/{index}") { backStack ->
+                val index = backStack.arguments?.getString("index")?.toIntOrNull() ?: 0
+
+                QuestionsScreen(
+                    viewModel = viewModel,
+                    index = index,
+                    onNext = {
+                        if (index + 1 < viewModel.questions.size) {
+                            navController.navigate(Screen.Question.createRoute(index + 1))
+                        } else {
+                            navController.navigate(Screen.Loading.route)
+                        }
+                    },
+                    onBack = if (index > 0) {
+                        { navController.navigate(Screen.Question.createRoute(index - 1)) }
+                    } else null)
+            }
+            composable(Screen.Result.route) {
+                ResultScreen(
+                    viewModel = viewModel,
+                    facts = viewModel.answers.map { it.questionId to it.selectedAnswer },
+                    onClick = {
+                        viewModel.reset()
+                        navController.navigate(Screen.Question.createRoute(0)) {
+                            popUpTo(Screen.Result.route) { inclusive = true }
+                        }
+                    }
+                )
+            }
         }
     }
 }
