459ace7ac04d16a541f51fba5a9ae4a9049b5f99
Rizki Sepriadi <muhammadrizkisepriadi@gmail.com>
2025-05-12 13:19:26 +0700

    feat: add loading, result and question path

--- app/src/main/java/com/app/sound_wise/ui/navigation/Screen.kt
+++ app/src/main/java/com/app/sound_wise/ui/navigation/Screen.kt
@@ -3,8 +3,9 @@ package com.app.sound_wise.ui.navigation
 sealed class Screen(val route: String) {
     object Home : Screen("home")
     object Splash : Screen("splash")
-    object Result: Screen("result")
-    object Detail : Screen("detail/{id}") {
-        fun createRoute(id: Int) = "detail/$id"
+    object Loading : Screen("loading")
+    object Result : Screen("result")
+    object Question : Screen("question") {
+        fun createRoute(index: Int): String = "$route/$index"
     }
 }
